aa3c79f4cf2da996d9fbea3a43d4751b3a23e483
Mikolaj Kos <mkk3@onet.pl>
2023-12-29 14:35:36 +0100

    fix grid bug (tailwind CSS dynamic classes)

--- components/play/blocks/block.js
+++ components/play/blocks/block.js
@@ -7,35 +7,55 @@ function Block(props) {
   const bgColorClass = isEven ? "bg-page2" : "bg-page4";
 
   let borderClasses = [];
+  let borderFixClasses = [
+    "border-t-4",
+    "border-r-4",
+    "border-b-4",
+    "border-l-4",
+  ];
+
   if (props.top) {
-    if (props.i !== 0) {
-      borderClasses.push("border-t-4");
-    }
+    borderClasses.push("border-t-4");
+    borderFixClasses = borderFixClasses.filter(
+      (className) => className !== "border-t-4"
+    );
   }
   if (props.right) {
-    if (props.j !== props.gridCount - 1) {
-      borderClasses.push("border-r-4");
-    }
+    borderClasses.push("border-r-4");
+    borderFixClasses = borderFixClasses.filter(
+      (className) => className !== "border-r-4"
+    );
   }
   if (props.bottom) {
-    if (props.i !== props.gridCount - 1) {
-      borderClasses.push("border-b-4");
-    }
+    borderClasses.push("border-b-4");
+    borderFixClasses = borderFixClasses.filter(
+      (className) => className !== "border-b-4"
+    );
   }
   if (props.left) {
-    if (props.j !== 0) {
-      borderClasses.push("border-l-4");
-    }
+    borderClasses.push("border-l-4");
+    borderFixClasses = borderFixClasses.filter(
+      (className) => className !== "border-l-4"
+    );
   }
   const borderClassesString = borderClasses.join(" ");
+  const borderFixClassessString = borderFixClasses.join(" ");
 
   return (
     <React.Fragment>
       <div
-        className={`col-span-1 row-span-1 bg-page1 overflow-hidden relative ${bgColorClass} ${borderClassesString} border-pageMenu`}
+        className={`col-span-1 row-span-1 overflow-hidden relative ${bgColorClass} ${borderClassesString} border-pageMenu`}
       >
-        <div>{props.i}</div>
-        <div>{props.j}</div>
+        <div
+          className={`w-full h-full ${borderFixClassessString} border-transparent flex items-center justify-center`}
+        >
+          <div className="w-full h-full p-1">
+            <div className="w-full h-full">
+              <div className="font-page text-[8px]">{props.i}</div>
+              <div className="font-page text-[8px]">{props.j}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
